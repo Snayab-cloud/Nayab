79cbc5a2543b8b1ee5703ba222e585b3d70056b4
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-13 15:59:50 +0530

    common

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -2,10 +2,10 @@
 
 USER_ID=$(id -u)
 
-if [ "$USER_ID" = 0 ]; then
-  echo -e "\e[31mYou must be a root user to execute the script\e[0m"
-fi
+#if [ "$USER_ID" = 0 ]; then
+#  echo -e "\e[31mYou must be a root user to execute the script\e[0m"
+#fi
 
 print() {
-  echo "\e[1m $(data +%c) \e[35m$(hostname)\e[0m \e[1;36m$(COMPONENT) :: $1"
+  echo -e "\e[1m $(data +%c) \e[35m$(hostname)\e[0m \e[1;36m$(COMPONENT) :: $1"
 }
