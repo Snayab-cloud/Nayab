bbfd3c7c8df96d72570be96e05fb5479f66e414c
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-13 16:25:39 +0530

    common

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -18,7 +18,7 @@ print() {
 }
 stat() {
   echo -e "[\e[1;34mINFO\e[0m]---------------------------------------------"
-  if ["$1" -eq 0 ]; then
+  if [ "$1" -eq 0 ]; then
   echo -e "[\e[1;34mINFO\e[0m] \e[1;32mSUCCESS \e[0m"
   else
   echo -e "[\e[1;34mINFO\e[0m] \e[1;31mFAILURE \e[0m"
