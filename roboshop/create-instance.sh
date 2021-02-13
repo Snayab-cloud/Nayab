1a2c9f4d201e4a56a2dc88210ed3086da9cdb27e
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-13 19:22:39 +0530

    common

---
+++ roboshop/create-instance.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z "${component}"]; then
+  echo "Need a Input of component name"
+  exit 1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-0835ecf0be1ed2f76 --tag-specifications "ResourceType=instance,Tags=[{Key-Name,Value=$(component)}]"
