56409be2d15eb0d05c0f07aeadcf787d105e2e09
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-15 17:16:56 +0530

    common

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,21 +8,25 @@ print "Installing nginx" "yum install nginx -y"
 yum install nginx -y
 stat $?
 
-print "Starting nginx service" "systemctl enable nginx"
-exit
-systemctl enable nginx
-systemctl start nginx
-stat $?
 
 print "Downloading frontend content" "curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 stat $?
 
+print "Download old docs" "cd /usr/share/nginx/html ; rm -rf *"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
-mv static/* .
-rm -rf static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+stat $?
 
+print "Extract Roboshop frontend Docs" "unzip /tmp/frontend.zip"
+unzip /tmp/frontend.zip && mv static/* . && rm -rf static README.md
+stat $?
+
+print "Setup Nginx config for Roboshp" "mv localhost.conf /etc/nginx/default.d/roboshop.conf"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+print "Starting nginx service" "systemctl enable nginx"
+exit
+systemctl enable nginx
+systemctl start nginx
+stat $?
