ce1cdfcbb2d92f5814712b0add17900c7fc0599d
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-15 17:40:40 +0530

    common

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -11,8 +11,7 @@ stat $?
 print "Downloading frontend content" "curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 stat $?
-
-print "Download old docs" "cd /usr/share/nginx/html ;rm -rf *"
+print "Remove old Docs" "cd /usr/share/nginx/html ;rm -rf *"
 cd /usr/share/nginx/html
 rm -rf *
 stat $?
