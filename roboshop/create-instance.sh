a2f42a7ba6130fa136535ed41feda421ea34a1ad
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-16 16:41:02 +0530

    common

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -9,7 +9,7 @@ fi
 
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
-if [STATE != "running" ]; then
+if [ STATE != "running" ]; then
 aws ec2 run-instances --launch-template LaunchTemplateId=lt-0835ecf0be1ed2f76 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 fi
 
