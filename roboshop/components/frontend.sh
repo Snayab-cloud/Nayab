44e26dfd3f3c165d7383b85713381126673cd6d8
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-16 11:30:26 +0530

    common

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,24 +8,25 @@ print "Installing nginx" "yum install nginx -y"
 yum install nginx -y
 stat $?
 
-print "Downloading frontend content" "curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+
+print "Downloading frontend content" 'curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"'
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 stat $?
 
-print "Remove old Docs" "cd /usr/share/nginx/html ;rm -rf *"
+print "Remove old docs" "cd /usr/share/nginx/html ;rm -rf *"
 cd /usr/share/nginx/html
 rm -rf *
 stat $?
 
-print "Extract Roboshop frontend Docs" "unzip /tmp/frontend.zip"
+print "Extract Roboshop frontend docs" "unzip /tmp/frontend.zip"
 unzip /tmp/frontend.zip && mv static/* . && rm -rf static README.md
 stat $?
 
-print "Setup Nginx config for Roboshp" "mv localhost.conf /etc/nginx/default.d/roboshop.conf"
+print "Setup nginx for Roboshop" "mv localhost.conf /etc/nginx/default.d/roboshop.conf"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-print"Starting nginx service" "systemctl start nginx"
-systemctl start nginx
+print "starting nginx service" "systemctl start nginx"
 systemctl enable nginx
+systemctl start nginx
 stat $?
