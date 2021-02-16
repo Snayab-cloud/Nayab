cdb528e0be4296f359b335675ff517bcc2fed459
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-16 16:39:22 +0530

    common

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -7,4 +7,16 @@ if [ -z "${component}" ]; then
   exit 1
 fi
 
+STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
+
+if [STATE != "running" ]; then
 aws ec2 run-instances --launch-template LaunchTemplateId=lt-0835ecf0be1ed2f76 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+fi
+
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+
+export component
+export IPADDRESS
+envsubst <record.json >/tmp/${component}.json
+
+#aws route53 change-resource-record-sets --hosted-zone-id Z00354771XF86GZCUQRB2 --change-batch file:///tmp/${component}.json
