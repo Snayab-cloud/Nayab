965d0748bd9e8064cb2904923be6438ed3d1bca8
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-17 21:55:34 +0530

    common

---
+++ roboshop/components/payment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+COMPONENT=payment
+
+source components/common.sh
+
+print "Install Python3" "yum install python36 gcc python3-devel -y"
+yum install python36 gcc python3-devel -y
+stat $?
+
+print "Create Roboshop user" "id roboshop || useradd roboshop"
+id roboshop || useradd roboshop
+stat $?
+
+print "Download payment code" 'curl -L -s -o /tmp/payment.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/cd32a975-ee45-4b3b-a08e-8e97c3ca7733/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"'
+curl -L -s -o /tmp/payment.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/cd32a975-ee45-4b3b-a08e-8e97c3ca7733/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+stat $?
+
+print "Extracting payment code" "mkdir -p /home/roboshop/payment && cd /home/roboshop/payment && unzip /tmp/payment.zip"
+mkdir -p /home/roboshop/payment && cd /home/roboshop/payment && unzip /tmp/payment.zip
+stat $?
+
+print "Install python dependencies" "cd /home/roboshop/payment && pip3 install -r requirements.txt"
+cd /home/roboshop/payment && pip3 install -r requirements.txt
+stat $?
+exit
+
+Update the roboshop user and group id in payment.ini file.
+
+Setup the service
+
+# mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
+# systemctl daemon-reload
+# systemctl enable payment
+# systemctl start payment
+
