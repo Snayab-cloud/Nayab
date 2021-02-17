167610cbdaf597ad6dbcd127e0e41f23946a2bc3
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-17 21:38:18 +0530

    common

---
+++ roboshop/components/rabbitmq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+COMPONENT=rabbitmq
+
+source components/common.sh
+
+print "Install Erlang" " yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y"
+yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y
+stat $?
+
+print "Setup RabbitMQ Repos" "curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+stat $?
+
+print "Install RabbitMQ" "yum install rabbitmq-server -y"
+yum install rabbitmq-server -y
+stat $?
+
+print "Start RabbitMQ service" "systemctl enable rabbitmq-server && systemctl start rabbitmq-server"
+systemctl enable rabbitmq-server && systemctl start rabbitmq-server
+stat $?
+
+print "Create application user in RabbitMQ" 'rabbitmqctl add_user roboshop roboshop123 && rabbitmqctl set_user_tags roboshop administrator && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"'
+rabbitmqctl add_user roboshop roboshop123 && rabbitmqctl set_user_tags roboshop administrator && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+stat $?
+
