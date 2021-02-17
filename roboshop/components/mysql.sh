6c229f26b3d1e358130db25a62265c9ec63c53c6
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-17 20:14:57 +0530

    common

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -21,24 +21,32 @@ systemctl enable mysqld
 systemctl start mysqld
 stat $?
 
-print "Grab Default MySQL password" "grep temp /var/log/mysqld.log"
-DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-stat $?
 
-echo "show databases" ; mysql -uroot -ppassword
-print "Reset MySQL password"
-mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'Default_Roboshop*999';
-uninstall plugin validate_password;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+echo "show databases" | mysql -uroot -ppassword &>/dev/null
+if [ $? -ne 0 ]; then
+    print "Grab Default MySQL password" "grep temp /var/log/mysqld.log"
+  DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  stat $?
+
+  print "Reset MySQL password"
+  mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" <<EOF
+  ALTER USER 'root'@'localhost' IDENTIFIED BY 'Default_Roboshop*999';
+  uninstall plugin validate_password;
+  ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 EOF
+  stat $?
+fi
+
+print "Download Scheme" 'curl -s -L -o /tmp/mysql.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/2235ab8a-3229-47d9-8065-b56713ed7b28/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"'
+curl -s -L -o /tmp/mysql.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/2235ab8a-3229-47d9-8065-b56713ed7b28/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 stat $?
 
+print "Load schema" "mysql <shipping.sql"
+cd /tmp
+unzip -o mysql.zip
+mysql -uroot -ppassword <shipping.sql
+stat $?
 
-# mysql_secure_installation
-
-
-# mysql -u root -p
 
 
 
